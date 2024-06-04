79f8e5ad9d5c18844bb4646651eb44d26244caa1
Harini <harinisree333@gmail.com>
2024-06-04 21:26:51 +0530

    the function call problem is done

---
+++ Day-03/FunctionCall.java
@@ -0,0 +1,13 @@
+//problem link : https://www.geeksforgeeks.org/problems/pass-by-reference-and-value/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=pass-by-reference-and-value
+
+class Solution {
+    static int[] passedBy(int a, int b) {
+        // code here
+        int[] arr = new int[2];
+
+        arr[0] = a + 1;
+        arr[1] = b + 2;
+
+        return arr;
+    }
+}
