23fde308714a5cfa4ed76169a91ba912c385c3cf
Harini <harinisree333@gmail.com>
2024-06-13 19:21:44 +0530

    Leader elements are printed

---
+++ Day-10/Leaders.java
@@ -0,0 +1,22 @@
+//problem : https://www.geeksforgeeks.org/problems/leaders-in-an-array-1587115620/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=leaders-in-an-array
+
+class Solution {
+    // Function to find the leaders in the array.
+    static ArrayList<Integer> leaders(int n, int arr[]) {
+        // Your code here
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        int maxi = Integer.MIN_VALUE;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (arr[i] >= maxi) {
+                list.add(arr[i]);
+                maxi = arr[i];
+            }
+        }
+
+        Collections.reverse(list);
+        return list;
+    }
+}
