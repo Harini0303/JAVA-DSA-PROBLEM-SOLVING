60bfd98db6e89f73041b6310773080cad10be732
Harini <harinisree333@gmail.com>
2024-06-21 21:45:20 +0530

    The key pair problem is updated

---
+++ Day-14/KeyPair.java
@@ -0,0 +1,19 @@
+//problem : https://www.geeksforgeeks.org/problems/key-pair5616/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=bottom_sticky_on_article
+
+class Solution {
+    boolean hasArrayTwoCandidates(int arr[], int n, int x) {
+        HashSet<Integer> seen = new HashSet<>();
+
+        for (int i = 0; i < n; i++) {
+            int complement = x - arr[i];
+
+            if (seen.contains(complement)) {
+                return true;
+            }
+
+            seen.add(arr[i]);
+        }
+
+        return false;
+    }
+}
