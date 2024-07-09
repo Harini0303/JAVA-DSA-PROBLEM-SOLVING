9b4e04397bfff854105834fad94bab3febfb01af
Harini <harinisree333@gmail.com>
2024-07-09 22:38:23 +0530

    The todays some problems are updated

---
+++ Day-29/LinkedListSoftware.java
@@ -0,0 +1,132 @@
+
+// Memory Usage
+// Nandeep is searching for a media software that uses least memory of his
+// computer system. He googles and finds a list of softwares, it was a long list
+// of softwares with its memory usage. He feels lazy to find the least memory
+// consuming software manually and decides to program a code that finds the
+// software with the least memory from the list.
+
+// Help nandeep iterate the list of softwares and get the software with least
+// memory.
+
+// Define structure
+// struct software {
+// char[15] name;
+// int memory;
+// struct software * link;
+// };
+
+// Include functions
+
+// void append ( struct software **, int ) ;
+// void display ( struct software * ) ;
+// int findmin (struct software *);
+// findmin --- to find the software which is taking least memory
+
+// Refer function specifications for further details.
+// [Note: The stmt. 'Elements in the linked list are' should be in the main
+// function.
+
+// Input and Output Format:
+// Refer sample input and output for formatting specifications.
+
+// [All text in bold corresponds to input and the rest corresponds to output.]
+
+// Sample Input and Output 1:
+// 1.Insert new software
+// 2.Display all the softwares
+// 3.Get the minimum memory software
+// 4.Exit
+// Enter your choice
+// 1
+// Enter the software name
+// chrome
+// Enter the memory used
+// 34
+// 1.Insert new software
+// 2.Display all the softwares
+// 3.Get the minimum memory software
+// 4.Exit
+// Enter your choice
+// 1
+// Enter the software name
+// winap
+// Enter the memory used
+// 54
+// 1.Insert new software
+// 2.Display all the softwares
+// 3.Get the minimum memory software
+// 4.Exit
+// Enter your choice
+// 2
+// Software list:
+// 1) chrome 34
+// 2) winap 54
+// 1.Insert new software
+// 2.Display all the softwares
+// 3.Get the minimum memory software
+// 4.Exit
+// Enter your choice
+// 3
+// Minimum memory Software is:
+// chrome 34
+// 1.Insert new software
+// 2.Display all the softwares
+// 3.Get the minimum memory software
+// 4.Exit
+// Enter your choice
+// 4
+// Exit
+
+// Sample Input and Output 2:
+// 1.Insert new software
+// 2.Display all the softwares
+// 3.Get the minimum memory software
+// 4.Exit
+// Enter your choice
+// 2
+// Software list is empty
+// 1.Insert new software
+// 2.Display all the softwares
+// 3.Get the minimum memory software
+// 4.Exit
+// Enter your choice
+// 1
+// Enter the software name
+// vlc
+// Enter the memory used
+// 65
+// 1.Insert new software
+// 2.Display all the softwares
+// 3.Get the minimum memory software
+// 4.Exit
+// Enter your choice
+// 1
+// Enter the software name
+// bluestack
+// Enter the memory used
+// 54
+// 1.Insert new software
+// 2.Display all the softwares
+// 3.Get the minimum memory software
+// 4.Exit
+// Enter your choice
+// 2
+// Software list:
+// 1) vlc 65
+// 2) bluestack 54
+// 1.Insert new software
+// 2.Display all the softwares
+// 3.Get the minimum memory software
+// 4.Exit
+// Enter your choice
+// 3
+// Minimum memory Software is:
+// bluestack 54
+// 1.Insert new software
+// 2.Display all the softwares
+// 3.Get the minimum memory software
+// 4.Exit
+// Enter your choice
+// 4
+// Exit
