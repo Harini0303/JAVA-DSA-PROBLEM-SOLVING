5101173ee07e30a6f2f169c97e56eee5eb72d7ff
Harini <harinisree333@gmail.com>
2024-08-15 21:11:59 +0530

    updated

---
+++ Day-67/LemonadeChange.java
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/lemonade-change/description/?envType=daily-question&envId=2024-08-15
+
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0, ten = 0;
+
+        for (int bill : bills) {
+            if (bill == 5) {
+                five++;
+            } else if (bill == 10) {
+                if (five > 0) {
+                    five--;
+                    ten++;
+                } else {
+                    return false;
+                }
+            } else {
+                if (ten > 0 && five > 0) {
+                    ten--;
+                    five--;
+
+                } else if (five >= 3) {
+                    five -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
