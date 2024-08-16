4f4b5c4b86d80611b8274ed2402218fe535db29a
Harini <harinisree333@gmail.com>
2024-08-16 21:21:46 +0530

    The day 68 problems are updated

---
+++ Day-68/Solution.java
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/reverse-bits/
+
+public class Solution {
+    // You need to treat n as an unsigned value
+    public int reverseBits(int n) {
+        int result = 0;
+
+        for (int i = 0; i < 32; i++) {
+            int bit = n & 1;
+
+            result = (result << 1) | bit;
+
+            n >>>= 1;
+        }
+
+        return result;
+    }
+}
