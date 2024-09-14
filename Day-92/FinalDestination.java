e9dad39a8d3520f2d2faed2ff9c82cfb8bea996b
Harini <harinisree333@gmail.com>
2024-09-14 20:11:34 +0530

    the day 92 problems are updated

---
+++ Day-92/FinalDestination.java
@@ -0,0 +1,14 @@
+// https://www.geeksforgeeks.org/problems/final-destination4628/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=practice_card
+
+class Solution {
+    static int canReach(Long a, Long b, Long x) {
+        // code here
+        Long minSteps = Math.abs(a) + Math.abs(b);
+
+        if (x >= minSteps && (x - minSteps) % 2 == 0) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+};
---
+++ Day-92/LongestSubarrayWithMaximumBitwiseAnd.java
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/longest-subarray-with-maximum-bitwise-and/description/?envType=daily-question&envId=2024-09-14
+
+class Solution {
+    public int longestSubarray(int[] nums) {
+        int max = 0;
+
+        for (int num : nums) {
+            max = Math.max(max, num);
+        }
+
+        int currentLength = 0;
+        int maxLength = 0;
+
+        for (int num : nums) {
+            if (num == max) {
+                currentLength++;
+                maxLength = Math.max(currentLength, maxLength);
+
+            } else {
+                currentLength = 0;
+            }
+        }
+        return maxLength;
+    }
+}
